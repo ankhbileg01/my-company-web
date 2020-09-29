17065fa774ce50ba42470388a31bc09a06dcc641
ankhbileg01 <Contact@brimsoft.mn>
2020-09-29 16:55:20 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,34 @@ module.exports = {
     author,
   },
   plugins: [
+<<<<<<< HEAD
     {
+=======
+    
+	{
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }]
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null
+          }
+        }
+      }
+    }
+	
+	{
+>>>>>>> parent of 402175a... Update gatsby-config.js
       resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId,
