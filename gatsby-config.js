52206c31aba367a3b39a5b34d5331f937a7986de
ankhbileg01 <Contact@brimsoft.mn>
2020-09-29 16:49:52 +0800

    Revert "Update gatsby-config.js"
    This reverts commit e7d5eadff849d5d8a3f09a518562c668960dfe84.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,6 @@
 const path = require("path");
 const { title, keywords, description, author, defaultLang, trackingId } = require("./config/site");
-const {
-  NODE_ENV,
-  URL: NETLIFY_SITE_URL = 'https://www.example.com',
-  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
-  CONTEXT: NETLIFY_ENV = NODE_ENV
-} = process.env;
-const isNetlifyProduction = NETLIFY_ENV === 'production';
-const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
 module.exports = {
   siteMetadata: {
     title,
