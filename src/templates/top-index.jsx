a6a9c1f9975f9a093318abeeaeb579fdb1225d33
Ankhbileg Ts <67893644+ankhbileg01@users.noreply.github.com>
2020-11-18 17:05:58 +0800

    up 11.18

--- src/templates/top-index.jsx
+++ src/templates/top-index.jsx
@@ -108,7 +108,7 @@ const IndexPage = ({ data, pathContext: { langKey, defaultLang, langTextMap } })
 
   return (
     <>
-      <SEO lang={langKey} title="Top" keywords={keywords} description={description} />
+      <SEO lang={langKey} title=""Программ хангамж үйлчилгээ"" keywords={keywords} description={description} />
       <Navbar
         anchors={anchors}
         frontmatter={navBarNode.frontmatter}
